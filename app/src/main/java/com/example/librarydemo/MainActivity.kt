c638f4aa4a55da3e9a3326ebbac728c1baa9a3ff
nitin tyagi <ideviftekhar@gmail.com>
2020-12-27 18:33:20 +0530

    Init commit

--- app/src/main/java/com/example/librarydemo/MainActivity.kt
+++ app/src/main/java/com/example/librarydemo/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.Toast
 import com.example.demosdklibrary.FirebaseLoginActivity
+import kotlinx.android.synthetic.main.activity_firebase_login.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
